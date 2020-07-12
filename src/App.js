81f401f2fa7263bfbd7ff97a7292048f02413774
Alena Zhmurko <alenazhmurko@gmail.com>
2020-07-12 18:41:40 +0300

    Add some fixes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
 import React, { useState, useEffect } from "react";
 
-import {Switch, Route, Link, useRouteMatch} from "react-router-dom";
+import {Switch, Route, Link, useRouteMatch, useLocation} from "react-router-dom";
 
 import "./styles.css";
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 export default function App() {
   return (
+    <>
       <Switch>
         <Route exact path="/" component={Films} />
         <Route path="/film/:id" component={Film} />
         <Route path="/starship/:id" component={Starship}/>
+        <Route path="/planet/:id" component={Planet}/>
       </Switch>
+    </>
   );
 }
 
 function Films() {
   const [films, setFilms] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchString, setSearchString] = useState(null);
   useEffect(() => {
     fetch("https://swapi.dev/api/films/")
       .then(response => response.json())
-      .then(res => setFilms(res.results));
+      .then(res => {setFilms(res.results); setSearchResult(res.results)});
   }, []);
+
+  const handleSubmit = (event) => {
+
+    var results = searchString ?
+                  films.filter((item) => {return item.title.includes(searchString)}) :
+                  films;
+    setSearchResult(results);
+    event.preventDefault();
+  }
+
+  const handleChange = (event) => {
+    setSearchString(event.target.value);
+  }
 
   return (
     <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Search:
+            <input type="text" value={searchString} onChange={handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
       <ul>
-        {films.map((film, i) => (
+        {searchResult.map((film, i) => (
           <li key={i}>
-            <Link to={{ pathname: `/film/${i}`, state: film }}>{film.title}</Link>
+            <Link to={{ pathname: `/film/${i+1}`, state: film }}>{film.title}</Link>
           </li>
         ))}
       </ul>
@@ -59,16 +90,25 @@ function Film() {
         </ul>
         
       </div>
+      <div>
+        <p>
+          <b>Planets</b>
+        </p>
+        <ul>
+          {film.planets.map((link, i) => <li key={i}><Link to={`/planet/${link.split('/')[5]}`}>/planet/{link.split('/')[5]}</Link></li>)}
+        </ul>
+        
+      </div>
       </div>) : ''}
     </>
   );
 }
 
-const renderShip = (ship) => {
-  const related_films = ship ? ship.films : [];
+const renderItem = (item, itemType) => {
+  const related_films = item ? item.films : [];
   return (
     <>
-      <h2>{ship.name}</h2>
+      <h2>{itemType}: {item.name}</h2>
       <div>
         Films:
         <ul>
@@ -92,7 +132,25 @@ function Starship() {
   
   return (
     <>
-      {ship ? renderShip(ship): ''}
+      {ship ? renderItem(ship, 'Starship'): ''}
+    </>
+  )
+}
+
+function Planet() {
+  let { url } = useRouteMatch();
+  let id = url.split('/')[2];
+
+  const [planet, setPlanets] = useState(null);
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/planets/${id}/`)
+      .then(response => response.json())
+      .then(res => setPlanets(res));
+  }, []);
+  
+  return (
+    <>
+      {planet ? renderItem(planet, "Planets"): ''}
     </>
   )
 }
